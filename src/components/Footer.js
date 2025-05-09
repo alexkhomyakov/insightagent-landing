e432f842a96c22c50d00182144660969431aa0a5
akhomyakov <alex.khomyakov@gmail.com>
2025-05-09 17:16:20 +0100

    netlify fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
       <div className="container mx-auto px-6 text-sm text-center">
         <p className="mb-4">© 2025 InsightAgent. All rights reserved.</p>
         <div className="flex justify-center space-x-4">
-          <a href="#" className="hover:text-white">Privacy Policy</a>
+          <a href="/privacy-policy" className="hover:text-white">Privacy Policy</a>
           <span>•</span>
-          <a href="#" className="hover:text-white">Terms</a>
+          <a href="/terms" className="hover:text-white">Terms</a>
         </div>
       </div>
     </footer>
